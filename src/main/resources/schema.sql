d4f57b142cf40e9cf65403cf3fcbe9cdfbae4770
rikuyu <rikuyu5884@gmail.com>
2025-01-23 19:01:56 +0900

    fix sql file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -23,15 +23,3 @@ CREATE TABLE IF NOT EXISTS borrow_records (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE
 );
-
--- INSERT INTO users (name, email) VALUES ('Sakura', 'sakura@email.com');
--- INSERT INTO books (title, published_date) VALUES ('Ruby on Rails Test Book', '2024-12-06');
--- INSERT INTO borrow_records (user_id, book_id, borrowed_date) VALUES (1, 1, '2024-12-14');
-
--- INSERT INTO users (name, email) VALUES ('Kenta', 'kenta@email.com');
--- INSERT INTO books (title, published_date) VALUES ('Golang Web Server Tutorial', '2022-02-11');
--- INSERT INTO borrow_records (user_id, book_id, borrowed_date) VALUES (2, 2, '2023-03-09');
-
--- INSERT INTO users (name, email) VALUES ('Yumi', 'yumi@email.com');
--- INSERT INTO books (title, published_date) VALUES ('Java Spring Boot Guide', '2023-01-20');
--- INSERT INTO borrow_records (user_id, book_id, borrowed_date) VALUES (3, 3, '2023-04-15');
