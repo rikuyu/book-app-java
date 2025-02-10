45e9a809cfb3fc3dade19bda708469a3eef410c4
rikuyu <rikuyu5884@gmail.com>
2025-02-11 00:02:21 +0900

    auth screen

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,34 +1,42 @@
-import {Link} from "react-router-dom";
-import {useState} from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-function Login() {
+function Register() {
     const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="flex flex-col items-center space-y-6 p-8 bg-white shadow-xl rounded-lg w-100 border border-gray-200">
-                <h1 className="text-2xl font-bold">Login Page</h1>
+                <h1 className="text-2xl font-bold">Register Page</h1>
                 <input
                     type="text"
                     placeholder="ユーザーネーム"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
-                    className="w-80 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 ring-green-200"
+                    className="w-80 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 ring-green-300"
+                />
+                <input
+                    type="email"
+                    placeholder="メールアドレス"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-80 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 ring-green-300"
                 />
                 <input
                     type="password"
                     placeholder="パスワード"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="w-80 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 ring-green-200"
+                    className="w-80 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 ring-green-300"
                 />
                 <button className="w-80 px-4 py-2 bg-green-600 text-white rounded-lg shadow-md hover:bg-green-500 transition">
-                    ログイン
+                    新規登録
                 </button>
-                <Link to="/register">
+                <Link to="/login">
                     <button className="w-80 px-4 py-2 border border-gray-400 text-gray-700 rounded-lg hover:bg-gray-100 transition">
-                        新規登録
+                        ログインへ戻る
                     </button>
                 </Link>
             </div>
@@ -36,4 +44,4 @@ function Login() {
     );
 }
 
-export default Login;
+export default Register;
