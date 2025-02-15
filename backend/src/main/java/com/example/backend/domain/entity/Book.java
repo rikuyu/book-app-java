4e2ec14d1bcacee16c2461c774487d217adc3d76
rikuyu <rikuyu5884@gmail.com>
2025-02-16 02:04:34 +0900

    replace normal class with record

--- backend/src/main/java/com/example/backend/domain/entity/Book.java
+++ backend/src/main/java/com/example/backend/domain/entity/Book.java
@@ -1,21 +1,14 @@
 package com.example.backend.domain.entity;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
-public class Book {
-    public Integer id;
-    public String title;
-    public Status status = Status.AVAILABLE;
-
-    @JsonCreator
-    public Book(String title) {
-        this.title = title;
-    }
-
-    public Book(Integer id, String title, Status status) {
-        this.id = id;
-        this.title = title;
-        this.status = status;
+public record Book(
+        Integer id,
+        String title,
+        Status status
+) {
+    public Book {
+        if (status == null) {
+            status = Status.AVAILABLE;
+        }
     }
 }
 
