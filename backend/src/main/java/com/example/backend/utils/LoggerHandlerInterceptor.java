4a1afb334fbafe5e40ef91de95d57e44699f11dd
rikuyu <rikuyu5884@gmail.com>
2025-03-02 18:05:29 +0900

    add interceptor

--- backend/src/main/java/com/example/backend/utils/LoggerHandlerInterceptor.java
+++ backend/src/main/java/com/example/backend/utils/LoggerHandlerInterceptor.java
@@ -2,6 +2,7 @@ package com.example.backend.utils;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,14 +12,18 @@ public class LoggerHandlerInterceptor implements HandlerInterceptor {
     Logger logger = LoggerFactory.getLogger(LoggerHandlerInterceptor.class);
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull Object handler
+    ) {
         String message = String.format(
                 "[%s] %-4s %s",
                 request.getLocalAddr(),
                 request.getMethod(),
                 request.getRequestURI()
         );
-        logger.debug(message);
+        logger.info(message);
         return true;
     }
 }
